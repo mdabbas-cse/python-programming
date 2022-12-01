7b5120a2c02f2ca5fb4d94eaa0c9bbf9cd1c6f2a
mdabbas-cse <gmabbas44@gmail.com>
2022-12-01 23:55:10 +0600

    string formatting

---
+++ 2.string-number/3.string-formatting.py
@@ -0,0 +1,5 @@
+# string formatting
+print("hello world {0}".format(1+1, 2+2))
+print("hello world {1}".format(1+1, 2+2))
+print("hello world {0} {1}".format(1+1, 2+2))
+print("hello world {a} {b}".format(a=1+1, b=2+2))
