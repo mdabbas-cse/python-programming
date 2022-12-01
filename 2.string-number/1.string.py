6a26506cef946ba1f0dffd7e68ad51a5a1449f1e
mdabbas-cse <gmabbas44@gmail.com>
2022-12-01 23:54:09 +0600

    string

---
+++ 2.string-number/1.string.py
@@ -0,0 +1,15 @@
+#string 
+print("hello world")
+
+# f string or formatted string
+print(f"hello world {1+1}")
+
+# string concatenation
+print("hello" + "world")
+print("hello" + " " + "world")
+
+# converting int to string
+print('hello' + str(50) + 'world')
+
+# string slicing
+print("hello world"[0:4])
