9d9fdf5bc3ce55aa5b2c63689e10d0648bed81ec
mdabbas-cse <gmabbas44@gmail.com>
2022-12-01 23:55:40 +0600

    sring method

---
+++ 2.string-number/4.string-method.py
@@ -0,0 +1,14 @@
+
+# string methods
+print("hello world".upper())
+print("HELLO WORLD".lower())
+print("hello world".capitalize())
+print("hello world".title())
+print("hello world".count("l")) # count the number of times a character appears in a string
+print("hello world".find("l")) # find the index of a character in a string
+print("hello world".replace("l", "L"))
+print("hello world".split(" "))
+print("hello world".split("l"))
+print("hello world".split("l", 1)) # split the string at the first occurrence of the character
+
+
