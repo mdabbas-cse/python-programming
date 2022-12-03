a44796b2576994a7386a6323d1014add716e02c7
mdabbas-cse <gmabbas44@gmail.com>
2022-12-04 00:03:26 +0600

    list

---
+++ 8.list/1.list.py
@@ -0,0 +1,6 @@
+# python list of numbers
+# input elements of list from user
+# split() function is used to split the string into list
+numbers = input("Enter a list of numbers: ")
+numbers = numbers.split(',')
+print(numbers)
