c6cfb0f38c2fa972786894e71719ab8960a1a548
mdabbas-cse <gmabbas44@gmail.com>
2022-12-04 01:54:28 +0600

    date and time modules

---
+++ 13.date-time/main.py
@@ -0,0 +1,6 @@
+import datetime
+
+# Get current date and time
+current_date_time = datetime.datetime.now()
+# Print current date and time
+print(datetime.datetime.strptime("02.12.2022", "%Y-%m-%d"))
